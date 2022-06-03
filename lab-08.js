54dafdf54689ce134afe7b04a567cbf066c32402
garnold-uo <105458607+garnold-uo@users.noreply.github.com>
2022-06-03 15:22:01 -0700

    Add files via upload

---
+++ lab-08.js
@@ -0,0 +1,59 @@
+// CIT281 - Lab 8
+// Garrett Arnold
+
+const fastify = require("fastify")();
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
+
+fastify.get("/photos", (request, reply) => {
+  fetch("https://jsonplaceholder.typicode.com/photos")
+    .then((response) => response.json())
+    .then((json) => {console.log(json);
+
+      reply
+      .code(200)
+      .header("Content-Type", "text/json; charset=utf-8")
+      .send({ error: "", statusCode: 200, photos: json});
+    })
+    .catch((error) => {
+      reply
+      .code(200)
+      .header("Content-Type", "text/json; charset=utf-8")
+      .send({ error: err, statusCode: 404, photos: []});
+    });
+});
+
+fastify.get("/photos/:id", (request, reply) => {
+  fetch("https://jsonplaceholder.typicode.com/photos")
+  .then((response) => response.json())
+  .then((json) => {console.log(json);
+
+    reply
+    .code(200)
+    .header("Content-Type", "text/json; charset=utf-8")
+    .send({ error: "", statusCode: 200, photos: json});
+  })
+  .catch((error) => {
+    reply
+    .code(200)
+    .header("Content-Type", "text/json; charset=utf-8")
+    .send({ error: err, statusCode: 404, photos: []});
+  });
+  
+  const { id = "" } = request.params;
+  reply
+    .code(200)
+    .header("Content-Type", "text/json; charset=utf-8")
+    .send({ error: "", statusCode: 200, photo: {json} });
+});
+
+// Start server and listen to requests using Fastify
+const listenIP = "localhost";
+const listenPort = 8080;
+fastify.listen(listenPort, listenIP, (err, address) => {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+  console.log(`Server listening on ${address}`);
+});
